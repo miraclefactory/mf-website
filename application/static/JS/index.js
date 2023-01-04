295eecc62e07b23092f7199b543839c061f45bcb
Emmanuel <emmanx25@gmail.com>
2023-01-05 00:54:33 +0100

    new commit for scroll to top button

--- application/static/JS/index.js
+++ application/static/JS/index.js
@@ -82,3 +82,13 @@ function closeBookmark()
     bookmark.opacity = 0;
     bookmark.transform = "translateX(100%)";
 }
+
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+
+    if(this.scrollY >= 560)
+    scrollTop.classList.add("scroll-top");
+    else scrollTop.classList.remove("scroll-top")
+}
+
+window.addEventListener("scroll", scrollTop)
