ec14eb3e0880aec976036db74e3e9425ce5fbca5
Yuelin Xin <sc20yx2@leeds.ac.uk>
2023-06-03 17:06:43 +0100

    statictify repo

--- tests/fixtures/app.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from application import create_app
-from flask import Flask
-
-
-@pytest.fixture(autouse=True)
-def app():
-    app, mail, db, migrate, pool = create_app()
-    yield app
